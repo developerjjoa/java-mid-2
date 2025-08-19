4b35372d8c6621fa654d094a29eb97c2020d39d2
developerjjoa <developerjjoa@gmail.com>
2025-08-20 01:36:24 +0900

    직접 구현하는 배열 리스트 4 - 제네릭 1 : 제네릭을 도입하면 타입 안정성을 확보하면서 문제를 한번에 해결할 수 있다. 제네릭은 자료를 보관하는 자료 구조에 가장 어울린다.

---
+++ src/collection/array/MyArrayListV4.java
@@ -0,0 +1,95 @@
+package collection.array;
+
+import java.util.Arrays;
+
+public class MyArrayListV4<E> {
+
+    private static final int DEFAULT_CAPACITY = 5;
+
+    private Object[] elementData;
+    private int size = 0;
+
+    public MyArrayListV4() {
+        elementData = new Object[DEFAULT_CAPACITY];
+    }
+
+    public MyArrayListV4(int initialCapacity) {
+        elementData = new Object[initialCapacity];
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void add(E e) {
+        if (size == elementData.length) {
+            grow();
+        }
+        elementData[size] = e;
+        size++;
+    }
+
+    public void add(int index, E e) {
+        if (size == elementData.length) {
+            grow();
+        }
+        //데이터 이동
+        shiftRightFrom(index);
+        elementData[index] = e;
+        size++;
+    }
+
+    private void shiftRightFrom(int index) {
+        for (int i = size; i > index; i--) {
+            elementData[i] = elementData[i - 1];
+        }
+    }
+
+    private void grow() {
+        int oldCapacity = elementData.length;
+        int newCapacity = oldCapacity * 2;
+        elementData = Arrays.copyOf(elementData, newCapacity);
+    }
+
+    @SuppressWarnings("unchecked")
+    public E get(int index) {
+        return (E) elementData[index];
+    }
+
+    public E set(int index, E element) {
+        E oldValue = get(index);
+        elementData[index] = element;
+        return oldValue;
+    }
+
+    public E remove(int index) {
+        E oldValue = get(index);
+        //데이터 이동
+        shiftLeftFrom(index);
+        size--;
+        elementData[size] = null;
+        return oldValue;
+    }
+
+    private void shiftLeftFrom(int index) {
+        for (int i = index; i < size - 1; i++) {
+            elementData[i] = elementData[i + 1];
+        }
+    }
+
+    public int indexOf(E o) {
+        for (int i = 0; i < size; i++) {
+            if (o.equals(elementData[i])) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public String toString() {
+        //[1,2,3,null,null] // size = 3
+        //[1,2,3] 값이 할당된 부분까지만 출력하고 싶다면
+        return Arrays.toString(Arrays.copyOf(elementData, size)) + " size=" + size + ", capacity=" + elementData.length;
+    }
+}
